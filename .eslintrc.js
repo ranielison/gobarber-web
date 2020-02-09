cb19bcf4572fff8ab2da9e921791f605e8f72f2d
Ranielison <ranielisonsoares2@gmail.com>
2020-02-09 01:07:48 -0300

    Configurando o babel plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,9 @@ module.exports = {
   },
   settings: {
     'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
       node: {
         paths: ['src', 'assets'],
       },
